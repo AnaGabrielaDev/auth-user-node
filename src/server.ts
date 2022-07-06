b00245ba5186382d70b475173230a09de747ecda
Ana Gabriela <anagabrieladev@gmail.com>
2022-07-05 22:39:57 -0300

    chore: add cors to project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { routes } from './routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
